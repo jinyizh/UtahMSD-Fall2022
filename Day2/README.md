58da78f1152886066f8f5c955fda58f9dc80db49
Jinyi Zhou <jinyi.zhou@utah.edu>
2022-08-23 13:10:55 -0600

    updated

---
+++ Day2/README.md
@@ -0,0 +1,6 @@
+This is the README file of Day2. This folder includes:
+1. Lab: Road Trip Calculator
+2. Group lab: Variables and Expressions, where I (Jinyi Zhou) did the 
+first part, Unit Conversion, and my parter, JAEHYUNG PARK, did the second 
+part, Gradebook.
+3. Assignment: Vending Machine (GiveChange).
---
+++ Day2/VariablesAndExpressionsUnitConversion/VariablesAndExpressionsGradebook.cpp
@@ -0,0 +1,32 @@
+// Part 2 of the Variables and Expressions Group Lab, done by JAEHYUNG 
+PARK
+
+#include <iostream>
+
+int main() {
+
+int first_assignment;
+int second_assignment;
+int third_assignment;
+int fourth_assignment;
+int fifth_assignment;
+
+std::cout << "Your first_assignment score is: ";
+std::cin >> first_assignment;
+std::cout << "Your second_assignment score is: ";
+std::cin >> second_assignment;
+std::cout << "Your third_assignment score is: ";
+std::cin >> third_assignment;
+std::cout << "Your fourth_assignment score is: ";
+std::cin >> fourth_assignment;
+std::cout << "Your fifth_assignment score is: ";
+std::cin >> fifth_assignment;
+
+
+float average = (float)(first_assignment + second_assignment + 
+third_assignment + fourth_assignment + fifth_assignment) / 5;
+std::cout << "Your all 5 assignments score average is " << average << 
+"\n";
+
+return 0;
+}
