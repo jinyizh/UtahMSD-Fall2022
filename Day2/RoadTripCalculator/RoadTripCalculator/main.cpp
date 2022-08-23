c95913d9b35f6ae18b72c701a539adc2ad9ec997
Jinyi Zhou <jinyi.zhou@utah.edu>
2022-08-23 11:44:22 -0600

    add day2 file to my repo

---
+++ Day2/RoadTripCalculator/RoadTripCalculator/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  RoadTripCalculator
+//
+//  Created by Jinyi Zhou on 8/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int distance;
+    float mpg;
+    float costOfGas;
+    
+    std::cout << "Enter the distance in whole miles:\n";
+    std::cin >> distance;
+    std::cout << "Enter the mpg efficiency:\n";
+    std::cin >> mpg;
+    std::cout << "Enter the cost of gas in dollars per gallon:\n";
+    std::cin >> costOfGas;
+    float costOfTrip = distance * costOfGas / mpg;
+    std::cout << "The cost of trip is " << costOfTrip << "\n";
+    
+    return 0;
+}
