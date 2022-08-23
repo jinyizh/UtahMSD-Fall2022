4586753436ee62afe48b6444cfd35aa84f86e554
Jinyi Zhou <jinyi.zhou@utah.edu>
2022-08-22 22:01:05 -0600

    second edition

--- Aug22/HelloWorld/HelloWorld/main.cpp
+++ Aug22/HelloWorld/HelloWorld/main.cpp
@@ -8,7 +8,6 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\nThis is Jinyi Zhou.\n";
     return 0;
 }
