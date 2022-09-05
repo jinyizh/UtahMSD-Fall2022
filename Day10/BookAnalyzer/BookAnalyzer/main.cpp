28d54e3ab02cc7910398b40687e6d6004ff43ae1
Jinyi Zhou <jinyi.zhou@utah.edu>
2022-09-05 02:32:37 -0600

    add HW

--- Day10/BookAnalyzer/BookAnalyzer/main.cpp
+++ Day10/BookAnalyzer/BookAnalyzer/main.cpp
@@ -106,31 +106,71 @@ string longestWord(vector<string> words) {
     return result;
 }
 
+void keyWord(string keyword, vector<string> words) {
+    int appearedTimes = 0;
+    int totalChars = totalChar(words);
+    vector<int> appearedCharLocations;
+    vector<int> appearedWordLocations;
+    int appearedCharLocation = 0;
+    for (int i = 0; i < words.size(); i++) {
+        if (words[i] == keyword) {
+            appearedTimes ++;
+            appearedCharLocations.push_back(appearedCharLocation);
+            appearedWordLocations.push_back(i);
+        }
+        appearedCharLocation += words[i].size();
+    }
+    if (appearedTimes == 0) {
+        cout << "The word " << keyword << " does not appear in the book." << endl;
+        return;
+    } else if (appearedTimes == 1) {
+        cout << "The word " << keyword << " appears 1 time " << endl;
+    } else {
+        cout << "The word " << keyword << " appears " << appearedTimes << " times" << endl;
+    }
+    for (int i = 0; i < appearedWordLocations.size(); i++) {
+        if (appearedWordLocations[i] != 0 &&
+            appearedWordLocations[i] != words.size() - 1) {
+            cout << "at " << static_cast<float> (appearedWordLocations[i]) / static_cast<float> (totalChars) * static_cast<float>(100) << "%: " <<
+            words[appearedWordLocations[i] - 1] << " " <<
+            words[appearedWordLocations[i]] << " " <<
+            words[appearedWordLocations[i] + 1] << endl;
+        } else if (appearedWordLocations[i] == 0) {
+            cout << "at " << static_cast<float> (appearedWordLocations[i]) / static_cast<float> (totalChars) * static_cast<float>(100) << "%: " <<
+            words[appearedWordLocations[i]] << " " <<
+            words[appearedWordLocations[i] + 1] << " " << endl;
+        } else if (appearedWordLocations[i] == words.size() - 1) {
+            cout << "at " << static_cast<float> (appearedWordLocations[i]) / static_cast<float> (totalChars) * static_cast<float>(100) << "%: " <<
+            words[appearedWordLocations[i] - 1] << " " <<
+            words[appearedWordLocations[i]] << " " << endl;
+        }
+    }
+    return;
+}
+
 int main(int argc, const char * argv[]) {
-    string input;
-    cout << "Enter a book you want to analyze: " << std::endl;
-    cin >> input;
-    ifstream fin(input + ".txt");
+    string input = argv[1];
+    ifstream fin(input);
     if (fin.fail()) {
         cout << "Failed to find the book!" << endl;
         return 0;
     }
-    
+
     string word;
     vector<string> allWords;
     while (fin >> word) {
         allWords.push_back(word);
     }
-    
+
     string title = findTitle(allWords);
     string author = findAuthor(allWords);
     string releaseDate = findReleaseDate(allWords);
-    
+
     int totalWords = allWords.size();
     int totalChars = totalChar(allWords);
-    
+
     string shortest = shortestWord(allWords);
-    
+
     cout << "title: " << title << endl;
     cout << "author: " << author << endl;
     cout << "release date: " << releaseDate << endl;
@@ -138,6 +178,8 @@ int main(int argc, const char * argv[]) {
     cout << "total characters: " << totalChars << endl;
     cout << "shortest word: " << shortest << endl;
     cout << "longest word: " << longestWord(allWords) << endl;
+    
+    keyWord(argv[2], allWords);
     
     return 0;
 }
