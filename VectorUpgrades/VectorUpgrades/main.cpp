b9989174d0cf525aa2b51191b4e5e4e9296b5d53
Jinyi Zhou <jinyi.zhou@utah.edu>
2022-09-19 00:57:50 -0600

    update vectorUpgrade

--- VectorUpgrades/VectorUpgrades/main.cpp
+++ VectorUpgrades/VectorUpgrades/main.cpp
@@ -7,54 +7,6 @@
 
 #include <iostream>
 
-//struct MyVec {
-//    int* data;
-//    int capacity;
-//    int size;
-//};
-//
-//MyVec makeVector(int initialCapacity) {
-//    MyVec vec;
-//    vec.capacity = initialCapacity;
-//    vec.size = 0;
-//    vec.data = new int[vec.size];
-//    return vec;
-//}
-//
-//void freeVector(MyVec vec) {
-//    delete [] vec.data;
-//    vec.data = nullptr;
-//}
-//
-//void pushBack(MyVec& vec, int i) {
-//    vec.data[vec.size] = i;
-//    vec.size++;
-//}
-//
-//void popBack(MyVec& vec) {
-//    vec.data[vec.size - 1] = NULL;
-//    vec.size--;
-//}
-//
-//int get(MyVec vec, int i) {
-//    return vec.data[i];
-//}
-//
-//void set(MyVec& vec, int i, int newValue) {
-//    vec.data[i] = newValue;
-//}
-//
-//void growVector(MyVec vec) {
-//    int* temp = new int[2 * vec.capacity];
-//    for (int i = 0; i < vec.size; i++) {
-//        temp[i] = vec.data[i];
-//    }
-//    delete [] vec.data;
-//    vec.data = temp;
-//    vec.capacity = 2 * vec.capacity;
-//    temp = nullptr;
-//}
-
 class MyVector {
 private:
     int* data;
@@ -97,7 +49,7 @@ public:
         this->data[this->size] = value;
         this->size++;
     }
-    void pop_back(int value) {
+    void pop_back() {
         this->data[this->size - 1] = NULL;
         this->size--;
     }
@@ -120,42 +72,49 @@ public:
 };
 
 int main(int argc, const char * argv[]) {
-//    // create a MyVec with capacity of 4 and size of 2
-//    MyVec vec1 = makeVector(4);
-//    int size = 2;
-//    for (int i = 0; i < size; i++) {
-//        vec1.data[i] = i; // value of elements
-//        vec1.size++;
-//    }
-//
-//    // test result:
-//    std::cout << "The elements in the struct vec1 are: " << std::endl;
-//    for (int i = 0; i < vec1.size; i++) {
-//        std::cout << vec1.data[i] << std::endl;
-//    }
-//
-//    pushBack(vec1, 4);
-//    std::cout << "After pushing back 4 to the struct, the elements in the struct vec1 are: " << std::endl;
-//    for (int i = 0; i < vec1.size; i++) {
-//        std::cout << vec1.data[i] << std::endl;
-//    }
-//
-//    popBack(vec1);
-//    std::cout << "After popping back, the elements in the struct vec1 are now: " << std::endl;
-//    for (int i = 0; i < vec1.size; i++) {
-//        std::cout << vec1.data[i] << std::endl;
-//    }
-//
-//    std::cout << "Get the 1st element in the struct using the get function: " << std::endl;
-//    int result = get(vec1, 1);
-//    std::cout << result << std::endl;
-//
-//    std::cout << "Set the 1st element to 2 in the struct using the set function: " << std::endl;
-//    set(vec1, 1, 2);
-//    std::cout << vec1.data[1] << std::endl;
     
     // initialize MyVector class
     int data[3] = {1, 2, 3};
     MyVector vector1 = MyVector(data, 3);
+    
+    // test results:
+    int sizeOfVector = vector1.getSize();
+    std::cout << "The elements in the class are: " << std::endl;
+    for (int i = 0; i < sizeOfVector; i++) {
+        int element = vector1.get(i);
+        std::cout << element << std::endl;
+    }
+    std::cout << "The vector (class) has a size of " << sizeOfVector << std::endl;
+    
+    vector1.push_back(4);
+    int newSizeOfVector = vector1.getSize();
+    std::cout << "After pushing back 4 to the class, the elements in the class vector1 are:" << std::endl;
+    for (int i = 0; i < newSizeOfVector; i++) {
+        int element = vector1.get(i);
+        std::cout << element << std::endl;
+    }
+    
+    vector1.pop_back();
+    int newSizeOfVector1 = vector1.getSize();
+    std::cout << "After popping back, the elements in the class vector1 are:" << std::endl;
+    for (int i = 0; i < newSizeOfVector1; i++) {
+        int element = vector1.get(i);
+        std::cout << element << std::endl;
+    }
+    
+    std::cout << "Get the 1st element in the class: " << std::endl;
+    std::cout << vector1.get(1) << std::endl;
+    
+    std::cout << "Set the 1st element to 6 in the class, the elements are now: " << std::endl;
+    vector1.set(1, 6);
+    int sizeOfVector2 = vector1.getSize();
+    for (int i = 0; i < sizeOfVector2; i++) {
+        int element = vector1.get(i);
+        std::cout << element << std::endl;
+    }
+    std::cout << "Set the capacity to 2." << std::endl;
+    vector1.setCapacity(2);
+    vector1.growVector();
+    std::cout << "After growing, the capacity is now: " << vector1.getCapacity() << std::endl;
     return 0;
 }
