052519412e9761eaf6311d16871993e6013f10d5
Jinyi Zhou <jinyi.zhou@utah.edu>
2022-10-07 13:02:00 -0600

    mixer works but too slow

--- CS6011/Week2/Day6/Synthesizer/src/main/java/com/example/synthesizer/Mixer.java
+++ CS6011/Week2/Day6/Synthesizer/src/main/java/com/example/synthesizer/Mixer.java
@@ -5,21 +5,31 @@ import java.util.ArrayList;
 public class Mixer implements AudioComponent{
     ArrayList<AudioComponent> inputs = new ArrayList<>();
 
+    public Mixer(AudioComponent input) {
+        this.inputs.add(input);
+    }
+
+    public Mixer(ArrayList<AudioComponent> inputs) { // more convenient
+        this.inputs = inputs;
+    }
+
     @Override
     public AudioClip getClip() {
         AudioClip result = new AudioClip();
-        if (this.inputs != null) {
+        if (this.inputs != null && this.inputs.size() != 0) {
             for (int i = 0; i < AudioClip.totalSample; i++) {
                 for (AudioComponent input : inputs) {
-                    if (result.getSample(i) + input.getClip().getSample(i) > Short.MAX_VALUE) { // clamping max
+                    AudioClip original = input.getClip();
+                    if (result.getSample(i) + original.getSample(i) > Short.MAX_VALUE) { // clamping max
                         result.setSample(i, Short.MAX_VALUE);
-                    } else if (result.getSample(i) + input.getClip().getSample(i) < Short.MIN_VALUE) { // clamping min
+                    } else if (result.getSample(i) + original.getSample(i) < Short.MIN_VALUE) { // clamping min
                         result.setSample(i, Short.MIN_VALUE);
                     } else {
-                        result.setSample(i, result.getSample(i) + input.getClip().getSample(i));
+                        result.setSample(i, result.getSample(i) + original.getSample(i));
                     }
                 }
             }
+            return result;
         }
         return null;
     }
--- CS6011/Week2/Day6/Synthesizer/test/com/example/synthesizer/MixerTest.java
+++ CS6011/Week2/Day6/Synthesizer/test/com/example/synthesizer/MixerTest.java
@@ -1,6 +1,5 @@
 package com.example.synthesizer;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.sound.sampled.AudioFormat;
@@ -8,39 +7,33 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 
-class FilterTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class MixerTest {
     @Test
     void simpleTest() throws LineUnavailableException {
         Clip c = AudioSystem.getClip();
         AudioFormat format16 = new AudioFormat(44100, 16, 1, true, false);
-        AudioComponent gen = new SineWave(220);
-        double scale = 0.1;
-        Filter vl = new Filter(scale);
-        vl.connectInput(gen);
+        AudioComponent wave0 = new SineWave(220);
+        AudioComponent wave1 = new SineWave(440);
 
-        AudioClip clip = vl.getClip();
+        Mixer mx = new Mixer(wave0);
+        mx.connectInput(wave1);
+
+        AudioClip clip = mx.getClip();
         byte[] data = clip.getData();
         c.open(format16, data, 0, data.length); // Reads data from our byte array to play it.
         System.out.println("About to play...");
         c.start();
-//        c.loop(2);
+//        for (int i = 0; i < AudioClip.totalSample; i++) {
+//            System.out.println(clip.getSample(i));
+//        }
+//        c.loop(0);
         // Makes sure the program doesn't quit before the sound plays.
         while (c.getFramePosition() < AudioClip.totalSample || c.isActive() || c.isRunning()) {
-            System.out.println(c.getFramePosition()); // Do nothing while we wait for the note to play.
+            System.out.println(c.getFramePosition());
         }
         System.out.println("Done.");
         c.close();
-    }
-
-    @Test
-    void thoroughTest() throws LineUnavailableException {
-        AudioComponent gen = new SineWave(220);
-        double scale = 0.1;
-        Filter vl = new Filter(scale);
-        vl.connectInput(gen);
-
-        for (int i = 0; i < AudioClip.totalSample; i += 20) { // would be too slow if i++
-            Assertions.assertEquals((int) Math.abs(vl.getClip().getSample(i) - scale * gen.getClip().getSample(i)), 0);
-        }
     }
 }
