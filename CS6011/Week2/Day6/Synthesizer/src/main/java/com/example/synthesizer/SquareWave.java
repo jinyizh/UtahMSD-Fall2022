4008472983fcc487e5390e86470bcee140d95593
Jinyi Zhou <jinyi.zhou@utah.edu>
2022-10-17 12:07:50 -0600

    corrected SqareWave

--- CS6011/Week2/Day6/Synthesizer/src/main/java/com/example/synthesizer/SquareWave.java
+++ CS6011/Week2/Day6/Synthesizer/src/main/java/com/example/synthesizer/SquareWave.java
@@ -4,11 +4,15 @@ public class SquareWave implements AudioComponent {
     int frequency; // in Hz
     final int maxValue = Short.MAX_VALUE; // max volume
 
+    public SquareWave(int frequency) {
+        this.frequency = frequency;
+    }
+
     @Override
     public AudioClip getClip() {
         AudioClip ac = new AudioClip();
         for (int i = 0; i < AudioClip.totalSample; i++) {
-            if ((double) (this.frequency * i / AudioClip.sampleRate) % 1 > 0.5) {
+            if (((double) this.frequency * i / AudioClip.sampleRate) % 1 > 0.5) { // beware of the location of casting
                 ac.setSample(i, this.maxValue);
             } else {
                 ac.setSample(i, -this.maxValue);
@@ -24,6 +28,10 @@ public class SquareWave implements AudioComponent {
 
     @Override
     public void connectInput(AudioComponent input) {
+        assert false;
+    }
 
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
     }
 }
---
+++ CS6011/Week3/Day11/HelloJS/helloJS.js
@@ -0,0 +1,9 @@
+// document.writeln("Hello World!"); // Display hello world in HTML page using document.writeln()
+console.log("Hello World!"); // Display hello world in HTML page using console.log()
+/* 
+The differences between using document.writeln() and using console.log() are:
+1. If using document.writelin(), the content is shown on the web page itself,
+   while console.log() only shows the content in the console
+   (on Safari, you have to open Develop -> Show Web Inspector -> console).
+2. Also, document.writelin() will overwrite the current web content, while console.log() will not.
+*/
