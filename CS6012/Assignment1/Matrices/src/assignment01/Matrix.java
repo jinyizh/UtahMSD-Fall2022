1681967e7d94d7c6efddf415ad0a31732bf3672d
Jinyi Zhou <jinyi.zhou@utah.edu>
2022-11-09 09:28:24 -0700

    update

--- CS6012/Assignment1/Matrices/src/assignment01/Matrix.java
+++ CS6012/Assignment1/Matrices/src/assignment01/Matrix.java
@@ -30,12 +30,14 @@ public class Matrix {
     }
     Matrix matrix = (Matrix) other; // if the above was not true, we know it's safe to treat 'o' as a Matrix
 
-    /*
-     * TODO: replace the below return statement with the correct code, you must
-     * return the correct value after determining if this matrix is equal to the
-     * input matrix
-     */
-    return false; // placeholder
+    // test if matrices have same dimensions
+    if (this.numRows != matrix.numRows || this.numColumns != matrix.numColumns) return false;
+    for (int i = 0; i < this.data.length; i++) { // loop through input's rows
+      for (int j = 0; j < this.data[i].length; j++) { // loop through elements in every row
+        if (this.data[i][j] != matrix.data[i][j]) return false; // if elements at same position don't equal
+      }
+    }
+    return true; // all elements equal, get out of for loop
   }
 
   @Override // instruct the compiler that we do indeed intend for this method to override
@@ -46,7 +48,17 @@ public class Matrix {
      * return a String that represents this matrix, as specified on the assignment
      * page
      */
-    return ""; // placeholder
+    String result = ""; // initialize result as empty string
+    for (int i = 0; i < data.length; i++) { // loop through rows
+      for (int j = 0; j < data[i].length; j++) { // loop through elements in every row
+        if (j == data[i].length - 1) {
+          result += data[i][j] + " \n"; // for the last element of every row, add new line
+        } else {
+          result += data[i][j] + " "; // elements that are not last element of every row
+        }
+      }
+    }
+    return result;
   }
 
   public Matrix times(Matrix matrix) {
@@ -57,6 +69,18 @@ public class Matrix {
      * resulting matrix, and fill it in with the correct values for matrix
      * multiplication
      */
+    if (this.numColumns == matrix.numRows) {
+      // Cij = Aix * Bxj - for reference
+      int[][] data = new int[this.numRows][matrix.numColumns];
+      for (int i = 0; i < data.length; i ++) { // loop through rows (same as lhs)
+        for (int j = 0; j < data[i].length; j++) { // loop through elements in row
+          for (int k = 0; k < matrix.data.length; k++) {
+            data[i][j] += this.data[i][k] * matrix.data[k][j];
+          }
+        }
+      }
+      return new Matrix(data);
+    }
     return null; // placeholder
   }
 
@@ -67,6 +91,15 @@ public class Matrix {
      * null. If they are compatible, create a new matrix and fill it in with the
      * correct values for matrix addition
      */
+    if (this.numRows == matrix.numRows && this.numColumns == matrix.numColumns) {
+      int[][] data = new int[this.numRows][this.numColumns];
+      for (int i = 0; i < data.length; i++) { // loop through rows
+        for (int j = 0; j < data[i].length; j++) { // loop through elements in every row
+          data[i][j] = this.data[i][j] + matrix.data[i][j]; // set data of result matrix
+        }
+      }
+      return new Matrix(data);
+    }
     return null; // placeholder
   }
 }
---
+++ CS6012/Assignment1/Matrices/src/assignment01/MatrixTest.java
@@ -0,0 +1,96 @@
+package assignment01;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixTest {
+    int[][] data0 = {{1, 1}, {1, 1}};
+    int[][] data1 = {{1, 2}, {3, 4}};
+    int[][] data2 = {{1, 2}, {3, 4}, {5, 6}};
+    int[][] data3 = {{4, 6}, {4, 6}};
+    int[][] data4 = {{1, 2, 3}, {4, 5, 6}};
+    int[][] data5 = {{0, 0}, {0, 0}};
+    int[][] data6 = {{-1, -1}, {-1, -1}};
+    int[][] data7 = {{1, 0}, {0, 1}};
+    int[][] data8 = {{1, 0}, {0, 1}, {0, 0}};
+    Matrix matrix0;
+    Matrix matrix1;
+    Matrix matrix2;
+    Matrix matrix3;
+    Matrix matrix4;
+    Matrix matrix5;
+    Matrix matrix6;
+    Matrix matrix7;
+    Matrix matrix8;
+
+    @BeforeEach
+    void setUp() {
+        matrix0 = new Matrix(data0);
+        matrix1 = new Matrix(data1);
+        matrix2 = new Matrix(data2);
+        matrix3 = new Matrix(data3);
+        matrix4 = new Matrix(data4);
+        matrix5 = new Matrix(data5);
+        matrix6 = new Matrix(data6);
+        matrix7 = new Matrix(data7);
+        matrix8 = new Matrix(data8);
+    }
+
+    @AfterEach
+    void tearDown() {
+        matrix0 = null;
+        matrix1 = null;
+        matrix2 = null;
+        matrix3 = null;
+        matrix4 = null;
+        matrix5 = null;
+    }
+
+    @Test
+    void testEquals() {
+        // don't simplify assertion - want to test the overridden equals method
+        assertEquals(false, matrix0.equals(matrix1));
+        assertEquals(false, matrix1.equals(matrix2));
+        int[][] data = {{1, 2}, {3, 4}, {5, 6}}; // has the same data as matrix2
+        Matrix matrix = new Matrix(data);
+        assertEquals(true, matrix2.equals(matrix));
+        assertEquals(true, matrix0.plus(matrix6).equals(matrix5)); // test both equals() and plus()
+        assertEquals(false, matrix7.equals(matrix8));
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("1 2 \n3 4 \n", matrix1.toString());
+        assertEquals("1 2 \n3 4 \n5 6 \n", matrix2.toString());
+        assertEquals("0 0 \n0 0 \n", matrix5.toString());
+        assertEquals(matrix5.toString(), matrix0.plus(matrix6).toString()); // test both toString() and plus()
+        assertEquals(matrix5.toString(), matrix5.times(matrix5).toString()); // test both toString() and times()
+    }
+
+    @Test
+    void times() {
+        assertEquals(null, matrix0.times(matrix2));
+        assertEquals(matrix3, matrix0.times(matrix1));
+        int[][] data = {{9, 12, 15}, {19, 26, 33}, {29, 40, 51}}; // matrix2 * matrix4
+        Matrix matrix = new Matrix(data);
+        assertEquals(matrix, matrix2.times(matrix4));
+        assertEquals(matrix5, matrix5.times(matrix5)); // {{0, 0}, {0, 0}}
+        assertEquals(matrix5, matrix5.times(matrix6)); // {{0, 0}, {0, 0}}
+        assertEquals(null, matrix7.times(matrix8)); // null
+    }
+
+    @Test
+    void plus() {
+        int[][] data = {{2, 3}, {4, 5}};
+        Matrix matrix = new Matrix(data);
+        assertEquals(matrix, matrix0.plus(matrix1));
+        assertEquals(matrix0, matrix0.plus(matrix5));
+        assertEquals(null, matrix0.plus(matrix2));
+        assertEquals(null, matrix2.plus(matrix4));
+        assertEquals(matrix5, matrix5.plus(matrix5)); // {{0, 0}, {0, 0}}
+        assertEquals(matrix5, matrix5.times(matrix5)); // test both plus() and times()
+    }
+}
