11740b8245a71b6ef6e45e5a69e4915351b113d9
Jinyi Zhou <jinyizhou21@gmail.com>
2023-01-14 13:05:10 -0700

    finished 6015 hw1

--- CS6015/MSDscript/cmdline.cpp
+++ CS6015/MSDscript/cmdline.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
-#include <string>
+#include <vector>
 #include "cmdline.h"
 
 void use_arguments(int argc, char* argv[]) {
     bool test_seen = false;
-    for (int i = 0; i < argc; i++) {
-        if (argv[i] == "--help") {
+    std::vector<std::string> argvs; // init string vec (argv is char* vec)
+    for (int i  = 0; i < argc; i++) {
+        argvs.__emplace_back(argv[i]); // suggested by clang to replace push_back
+    }
+    for (int i = 1; i < argc; i++) {
+        if (argvs[i] == "--help") { // comparison of argv[i] and a string is not implicit
             std::cout << "available commands:" << std::endl;
             std::cout << "--test: the only command that is allowed" << std::endl;
             std::cout << "no other commands are allowed so far" << std::endl;
             exit(0);
         }
-        else if (argv[i] == "--test") {
+        else if (argvs[i] == "--test") {
             if (!test_seen) {
                 std::cout << "Tests passed" << std::endl;
                 test_seen = true;
             } else {
-                std::cerr << "already tested";
+                std::cerr << "already tested" << std::endl;
                 exit(1);
             }
         }
